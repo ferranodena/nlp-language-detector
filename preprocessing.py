5385c925a4dd8e942d6d26e0d6fd624ae17adade
ferranodena <ferran.odena@estudiantat.upc.edu>
2026-02-20 19:25:09 +0100

    dades i poco mas

---
+++ preprocessing.py
@@ -0,0 +1,9 @@
+
+
+def preprocess_text():
+    """This function:
+    1. Delete the digits
+    2. Convert to lowercase
+    3. Subtitute the continuous spaces with a single space
+    4. Concatenate all the sentences using a double space as a separator
+    """
